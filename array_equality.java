995a82784da0b867f148ac81560d7513422fdc14
Suyas12 <109739678+Suyas12@users.noreply.github.com>
2022-08-04 17:50:01 +0530

    Update array_equality.java

--- array_equality.java
+++ array_equality.java
@@ -5,8 +5,8 @@ class array_equality
 {
     public static void main(String args[]) 
     {
-        int [] array1 = new int[] {7,12,-4,8,10};
-        int [] array2 = new int[] {7,12,-4,8,10};
+        int [] array1 = {7,12,-4,8,10};
+        int [] array2 = {7,12,-4,8,10};
         System.out.println(Arrays.equals(array1,array2));
     }
     
